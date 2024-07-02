52bb2bd73e65a50e33d81fd697f4bed0dcedff1a
JStivenJ <jsjimenezu@unal.edu.co>
2024-07-02 10:11:19 -0500

    Update stations.py
    Código stations, ingresan (Longitud, Latitud)
    return diccionario con {llave = nombre emisora: valor = frecuencia}

--- gcpds/em_spectrum_monitor/utils/stations.py
+++ gcpds/em_spectrum_monitor/utils/stations.py
@@ -1,9 +1,12 @@
 from typing import List, Tuple
 
+import numpy as np
+import pandas as pd 
+from geopy.geoconders import Nominatim
 
 # ----------------------------------------------------------------------
-def stations(gps_position: Tuple[int, int, int], n: int = 10) -> List[Dict[str, str]]:
-    """Returns a list of dictionaries containing information about the n closest stations to the given GPS position.
+def stations(Longitude, Latitude):
+    """sReturns a list of dictionaries containing information about the n closest stations to the given GPS position.
 
     Parameters
     ----------
@@ -26,9 +29,61 @@ def stations(gps_position: Tuple[int, int, int], n: int = 10) -> List[Dict[str, 
      {'name': 'Station4', 'frequency': '104.4 Hz'},
      {'name': 'Station5', 'frequency': '105.5 Hz'}]
     """
+    def Coordinates (city, department):
+        geo = Nominatim (user_agent = "Stations", timeout = 2)
+        try: 
+            lugar = geo.geocode(f"{city}, {department}, Colombia")
+            if lugar is not None:
+                longitud = lugar.longitude
+                latitud = lugar.latitude
+                return longitud, latitud
+            else:
+                print (f"No se encontraron coordenadas para {city}.")
+                return None, None
+        except GeocoderTimedOut:
+            print (f"Geocoding para {city} ha superado el tiempo de espera.")
+            return None, None
+        
+        except AttributeError:
+            print (f"Error al obtener coordenadas para {city}.")
+            return None, None
 
+    def Haversine (lon_1, lat_1, lon_2, lat_2):
+        lon_1, lat_1, lon_2, lat_2 = map(np.radians, [lon_1, lat_1, lon_2, lat_2])
+
+        dlon = lon_2 - lon_1
+        dlat = lat_2 - lat_1
+        a = np.sin(dlat / 2)**2 + np.cos(lat_1) * np.cos(lat_2) * np.sin(dlon / 2)**2
+        c = 2 * np.arcsin(np.sqrt(a))
+        r = 6371
+        return c * r
+
+    url = "https://raw.githubusercontent.com/Redes-de-Monitoreo-Multiproposito/python-gcpds.em_spectrum_monitor/main/Radioemisoras_Coordenadas.csv"
+
+    try:
+        df = pd.read_csv(url, on_bad_lines = 'skip')
+    except pd.errors.ParserError as e:
+        print (f"Error al analizar el archivo CSV: {e}")
+
+    df = df[df['BANDA'] != 'AM']
+
+    unique_cities = df.drop_duplicates(subset=['CIUDAD'])
+
+    cities_ = unique_cities['CIUDAD'].tolist()
+    department_ = unique_cities['DEPARTAMENTO'].tolist()
+
+    lon_given, lat_given = Longitude, Latitude
+    df['Distancia_km'] = df.apply(lambda row: Haversine(lon_given, lat_given, row['LONGITUD'], row['LATITUD']), axis=1)
+
+    ciudades_cercanas = df[df['Distancia_km'] < 30].reset_index(drop=True)
+
+    Station = {}
+    emisora = ciudades_cercanas['NOMBRE EMISORA']
+    frecue = ciudades_cercanas['FRECUENCIA']
+    Station = dict(zip(emisora, frecue))
+    
     # PSS: Sample implementation to demonstrate function behavior
     # In a real-world scenario, this would be replaced with the actual logic
     # to determine the closest stations based on the GPS position.
-    return [f'Station{i+1}' for i in range(n)]
+    return [f'Station{i+1}' for i in range(len(Station))]
 
